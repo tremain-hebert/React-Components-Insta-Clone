34204d2ab16d12ed1f12a3c190594ad2b7efdb3b
Truman <truman@Trumans-iMac.local>
2020-06-21 21:58:54 -0500

    Completed MVP

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,13 +3,18 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+import dummyData from "/Users/truman/Documents/Lambda/React-Components-Insta-Clone/src/dummy-data.js";
 
 const CommentSection = props => {
+  const [data, setData] = useState(props);
+  console.log(data);
   // Add state for the comments
 
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {data.comments.map(c => (
+        <Comment key={c.postId} comment={c} />
+      ))}
       <CommentInput />
     </div>
   );
